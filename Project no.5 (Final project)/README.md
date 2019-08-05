042b11fc20f04180a82a75027b8f86199a5f9d9b
Peh Jun Siang <austin9090@hotmail.com>
2019-08-06 04:24:03 +0800

    Revert "Revert "update README""
    This reverts commit fb5ad1fb11a090825030b7f29421dfa3dc8e8dba.

--- Project no.5 (Final project)/README.md
+++
@@ -1,6 +0,0 @@
-﻿# Project no.5 (Final project)
-Database: MongoDB using mongoose object modelling (noSQL)
-Backend: nodejs with Express framework, EJS template
-CSS framework: bootstrap v4.3.1
-images: unsplash.com
-
--- Project no.5 (Final project)/models/userModel.js
+++
@@ -1,8 +0,0 @@
-﻿const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
-var userSchema = new mongoose.Schema({
-	username: String,
-	password:String
-})
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User", userSchema);
